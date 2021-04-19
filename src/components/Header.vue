9291b80a6f64b4e53c61619ac1324fd4ff3b45a5
Sardo Ip <sardo.ip@sardo.work>
2021-04-19 23:08:18 +0800

    Init commit

---
+++ src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="header right dark sticky">
+    <div class="container">
+      <!-- Logo -->
+      <div class="header-logo">
+        <a href="#">
+          <img class="logo-dark" src="../assets/logo-white.png" alt="">
+          <img class="logo-light" src="../assets/logo.png" alt="">
+        </a>
+      </div>
+      <!-- Menu -->
+      <div class="header-menu">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">关于Neverland</a>
+            <ul class="nav-dropdown">
+              <li class="nav-dropdown-item"><a class="nav-dropdown-link" href="#">关于Neverland</a></li>
+              <li class="nav-dropdown-item"><a class="nav-dropdown-link" href="#">如何游玩？</a></li>
+              <li class="nav-dropdown-item"><a class="nav-dropdown-link" href="#">FAQ</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">资料库</a>
+            <ul class="nav-dropdown">
+              <li class="nav-dropdown-item"><a class="nav-dropdown-link" href="#">角色卡</a></li>
+              <li class="nav-dropdown-item"><a class="nav-dropdown-link" href="#">全部剧情</a></li>
+              <li class="nav-dropdown-item"><a class="nav-dropdown-link" href="#">角色相关剧情</a></li>
+              <li class="nav-dropdown-item"><a class="nav-dropdown-link" href="#">活动剧情</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">剧照</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">版权声明</a>
+          </li>
+        </ul>
+      </div>
+      <!-- Menu Toggle -->
+      <button class="header-toggle">
+        <span></span>
+      </button>
+    </div><!-- end container -->
+  </div>
+  <!-- end Header -->
+</template>
+
+<style lang="scss">
+.nav-item{
+  a{
+    font-size: 1.1em;
+    font-weight: bold;
+  }
+}
+</style>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <div class="owl-carousel owl-nav-overlay owl-dots-overlay" data-owl-autoplay="true" data-owl-nav="true" data-owl-dots="true" data-owl-items="1">
+      <!-- Slider Item 1 -->
+      <div class="section-2xl bg-image" data-bg-src="../assets/carousel.jpg">
+        <div class="bg-black-06">
+          <div class="container text-center">
+            <h6 class="font-small font-weight-normal uppercase margin-bottom-20">Lifestyle, Fashion</h6>
+            <h1>Hero Post with Image</h1>
+            <a class="button button-lg button-radius button-border-2 button-reveal-right-outline-white margin-top-20" href="#"><i class="ti-arrow-right"></i><span>Read More</span></a>
+          </div>
+        </div>
+      </div>
+    </div><!-- end owl-carousel -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components:{
+    Header,
+  },
+  mounted(){
+    $(".bg-image").each(function() {
+      var bgData = $(this).attr("data-bg-src");
+      $(this).css('background-image', 'url("' + bgData + '")');
+    });
+  },
 }
 </script>
