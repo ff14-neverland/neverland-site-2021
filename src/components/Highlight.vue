7e8c5fd3834eb7b2da6f1a345946a52aac1369f5
Sardo Ip <sardo.ip@sardo.work>
2021-06-13 01:16:00 +0800

    Add swiper

--- src/components/Highlight.vue
+++ src/components/Highlight.vue
@@ -5,10 +5,38 @@
         <div class="row">
           <div class="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2">
             <h2>精選劇情</h2>
-
+            <swiper ref="mySwiper">
+              <swiper-slide>Slide 1</swiper-slide>
+              <swiper-slide>Slide 2</swiper-slide>
+              <swiper-slide>Slide 3</swiper-slide>
+              <swiper-slide>Slide 4</swiper-slide>
+              <swiper-slide>Slide 5</swiper-slide>
+              <div class="swiper-pagination" slot="pagination"></div>
+            </swiper>
           </div>
         </div>
       </div>
     </div><!-- end container -->
   </div>
 </template>
+
+<script>
+import Header from '@/components/Header.vue'
+import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css';
+
+export default {
+  name: 'Highlight',
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  directives: {
+    swiper: directive
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,18 +3,7 @@
     <Header />
     <Banner />
     <!-- section -->
-		<div id="first" class="section-lg">
-			<div class="container text-center">
-				<div class="margin-bottom-70">
-					<div class="row">
-						<div class="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2">
-							<h2>精選劇情</h2>
-
-						</div>
-					</div>
-				</div>
-			</div><!-- end container -->
-		</div>
+    <Highlight />
 		<!-- end section -->
   </div>
 </template>
@@ -22,12 +11,14 @@
 <script>
 import Header from '@/components/Header.vue'
 import Banner from '@/components/Banner.vue'
+import Highlight from '@/components/Highlight.vue'
 
 export default {
   name: 'Home',
   components:{
     Header,
     Banner,
+    Highlight,
   },
 }
 </script>
